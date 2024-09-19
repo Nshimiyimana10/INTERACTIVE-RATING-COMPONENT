78053ae649f4091cb7e54d8fa9aaeb3aef756244
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-09-19 11:59:51 -0700

    added

---
+++ index.js
@@ -0,0 +1,27 @@
+const button1 = document.querySelector('#btn-1');
+const button2 = document.querySelector('#btn-2');
+const button3 = document.querySelector('#btn-3');
+const button4 = document.querySelector('#btn-4');
+const button5 = document.querySelector('#btn-5');
+
+button1.addEventListener("click", () =>{
+    button1.style.backgroundColor = 'hsl(25,97%,53%)';
+    
+})
+
+button2.addEventListener("click", () =>{
+    button2.style.backgroundColor = 'hsl(25,97%,53%)';
+    
+})
+button3.addEventListener("click", () =>{
+    button3.style.backgroundColor = 'hsl(25,97%,53%)';
+    
+})
+button4.addEventListener("click", () =>{
+    button4.style.backgroundColor = 'hsl(25,97%,53%)';
+    
+})
+button5.addEventListener("click", () =>{
+    button5.style.backgroundColor = 'hsl(25,97%,53%)';
+    
+})
